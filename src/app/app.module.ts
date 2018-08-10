d862a5230711ffb35744e06ca5510806da6a27cc
Kostadin Prosenikov <voodooto@gmail.com>
2018-08-10 09:19:46 +0300

    Initial add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HighLightDirective } from './directives/highlight.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HighLightDirective
   ],
   imports: [
     BrowserModule
