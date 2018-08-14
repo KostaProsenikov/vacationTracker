2b2491777f52865d0c241b24ee40465688a975b1
Kostadin Prosenikov <voodooto@gmail.com>
2018-08-14 14:51:52 +0300

    Add users table

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UsersService } from '../services/users.service';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-users',
@@ -9,6 +10,8 @@ import { UsersService } from '../services/users.service';
 export class UsersComponent implements OnInit {
 
   users: any[] = [];
+  displayedColumns = ['firstName', 'lastName', 'username', 'email', 'age', 'daysLeft'];
+  dataSource       = new MatTableDataSource();
 
   constructor(private userService: UsersService) { }
 
@@ -16,11 +19,16 @@ export class UsersComponent implements OnInit {
     this.getAllUsers();
   }
 
+  applyFilter(filterValue: string) {
+      this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
   getAllUsers() {
     this.userService.getUsers().subscribe(
       (res: Array<any>) => {
-        console.log('res', res);
+        // console.log('res', res);
         this.users = res;
+        this.dataSource   = new MatTableDataSource(this.users);
       },
       (err) => this.onError(err)
     )
